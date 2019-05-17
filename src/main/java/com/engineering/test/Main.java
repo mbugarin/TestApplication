22d9c3d565b2726e450210b48b1bda717eb9c19c
Milos Bugarin <milos.bugarin@eng.it>
2019-05-17 12:51:16 +0200

    Changing the Main class to print name characters in specific format

--- src/main/java/com/engineering/test/Main.java
+++ src/main/java/com/engineering/test/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         String name = "Milos";
-        name.chars().forEach(i -> System.out.println((char)i));
+        printEachLetter(name);
+        name = "Janko";
+        printEachLetter(name);
+    }
+
+    private static void printEachLetter(String value) {
+        value.chars().forEach(i -> System.out.println((char)i));
+        System.out.println("\n");
     }
 }
