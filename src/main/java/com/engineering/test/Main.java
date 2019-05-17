229a731d3a8459dd4b092ec14684a628a6c82e98
Milos Bugarin <milos.bugarin@eng.it>
2019-05-17 12:57:12 +0200

    Added logic for summing two numbers

--- src/main/java/com/engineering/test/Main.java
+++ src/main/java/com/engineering/test/Main.java
@@ -5,5 +5,14 @@ public class Main {
     public static void main(String[] args) {
         String name = "Milos";
         name.chars().forEach(i -> System.out.println((char)i));
+
+        int numeroUno = 22;
+        int numeroDue = 55;
+
+        System.out.println(String.format("Sum of numbers %d and %d is %d", numeroUno, numeroDue, sumNumbers(numeroUno, numeroDue)));
+    }
+
+    private static long sumNumbers(int num1, int num2) {
+        return num1 + num2;
     }
 }
