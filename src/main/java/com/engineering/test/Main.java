d2c5dd4e487dbdb86cd3ec35eef95ab3bc32de2a
Milos Bugarin <milos.bugarin@eng.it>
2019-05-17 12:43:00 +0200

    Created Main class and setting up project structure

---
+++ src/main/java/com/engineering/test/Main.java
@@ -0,0 +1,9 @@
+package com.engineering.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Milos";
+        name.chars().forEach(i -> System.out.println((char)i));
+    }
+}
